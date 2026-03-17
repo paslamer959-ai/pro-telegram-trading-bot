6637711dcf03e9b89dcb471f99b4d69abc3d7612
paslamer959-ai <paslamer959@gmail.com>
2026-03-17 22:09:41 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    TOKEN: "7715705527:AAHimpn_L_YU4Vr_gTIjvJH1rhRVwxwN3Hk",
+    MONGO_URL: "mongodb+srv://qais:qais959@cluster0.ggc3dpc.mongodb.net/TradingPro?retryWrites=true&w=majority",
+    WEBAPP_URL: "https://pro-telegram-trading-bot.onrender.com"
+};
